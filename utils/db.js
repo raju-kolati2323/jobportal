ddf5164d2f23f36ac468623839a5703099c8471d
raju-kolati2323 <raju.kolati5@gmail.com>
2024-10-07 12:27:56 +0530

    third commit

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    const localUri = 'mongodb://localhost:27017/jobportal'
+    // const localUri = 'mongodb://localhost:27017/jobportal'
+    const localUri="mongodb+srv://satyainnowide:jobportal@jobportal.otswd.mongodb.net/?retryWrites=true&w=majority&appName=JobPortal"
     try {
         await mongoose.connect(localUri);
         console.log('mongodb connected successfully');
