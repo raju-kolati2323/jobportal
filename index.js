efa5d45447b32a4b8a88e871c69f9b4a339e56a2
raju-kolati2323 <raju.kolati5@gmail.com>
2024-10-07 11:56:41 +0530

    second commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobportal-0.web.app/',
+    origin:["http://localhost:5173","https://jobportal-01.web.app/"],
     credentials:true
 }
 
--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    const localUri = 'mongodb+srv://satyainnowide:jobportal@jobportal.uouvx.mongodb.net/?retryWrites=true&w=majority&appName=jobPortal';
-   // process.env.MONGO_URI
+    const localUri = 'mongodb://localhost:27017/jobportal'
     try {
         await mongoose.connect(localUri);
         console.log('mongodb connected successfully');
