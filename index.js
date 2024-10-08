1127173ac7fbb486f449820809540a120e760bf5
raju-kolati2323 <raju.kolati5@gmail.com>
2024-10-08 11:17:02 +0530

    seventh commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:["http://localhost:5173","https://jobportal-01.web.app/"],
+    origin:'http://localhost:5173',
     credentials:true
 }
 
