3dcb69ec52d6dd3529ec34ccabbe2f60b2cb482c
raju-kolati2323 <raju.kolati5@gmail.com>
2024-10-08 12:36:12 +0530

    'change1'

--- routes/application.route.js
+++ routes/application.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route("/apply/:id").get( applyJob);
 router.route("/get").get(isAuthenticated, getAppliedJobs);
-router.route("/:id/applicants").get(isAuthenticated, getApplicants);
+router.route("/:id/applicants").get( getApplicants);
 router.route("/status/:id/update").post(isAuthenticated, updateStatus);
  
 
