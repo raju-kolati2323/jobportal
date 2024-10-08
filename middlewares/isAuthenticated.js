540002febbb48c0bf624b86054415b702deb471d
raju-kolati2323 <raju.kolati5@gmail.com>
2024-10-08 11:19:13 +0530

    first commit

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
-    
+    try {
         const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({
@@ -9,7 +9,7 @@ const isAuthenticated = async (req, res, next) => {
                 success: false,
             })
         }
-        try {
+  
         const decode = await jwt.verify(token,process.env.SECRET_KEY);
         if(!decode){
             return res.status(401).json({
