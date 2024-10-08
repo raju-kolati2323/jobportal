73a0b40ae55c0e48831fc293ea2ae2f15e072c4e
raju-kolati2323 <raju.kolati5@gmail.com>
2024-10-08 14:29:33 +0530

    'change'

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,26 +1,42 @@
 import jwt from "jsonwebtoken";
 
-const isAuthenticated = async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.status(401).json({
-                message: "User not authenticated",
-                success: false,
-            })
-        }
+// const isAuthenticated = async (req, res, next) => {
+//     try {
+//         const token = req.cookies.token;
+//         if (!token) {
+//             return res.status(401).json({
+//                 message: "User not authenticated",
+//                 success: false,
+//             })
+//         }
   
-        const decode = await jwt.verify(token,process.env.SECRET_KEY);
-        if(!decode){
-            return res.status(401).json({
-                message:"Invalid token",
-                success:false
-            })
-        };
-        req.id = decode.userId;
-        next();
-    } catch (error) {
-        console.log(error);
+//         const decode = await jwt.verify(token,process.env.SECRET_KEY);
+//         if(!decode){
+//             return res.status(401).json({
+//                 message:"Invalid token",
+//                 success:false
+//             })
+//         };
+//         req.id = decode.userId;
+//         next();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+const isAuthenticated = (req, res, next) => {
+    const token = req.cookies.token; // or however you're sending the token
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
     }
-}
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        req.id = decoded.userId; // Assuming you store userId in token
+        next();
+    });
+};
 export default isAuthenticated;
