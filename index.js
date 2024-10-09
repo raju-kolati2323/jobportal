85cc69e623212b5522bbfd78bf508d373e0b068b
raju-kolati2323 <raju.kolati5@gmail.com>
2024-10-09 15:30:42 +0530

    deploy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:['http://localhost:5173','https://jobportal-02.web.app/'],
+    origin:["http://localhost:5173","https://jobportal-03.web.app"],
     credentials:true
 }
 
