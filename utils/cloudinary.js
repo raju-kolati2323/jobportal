2185a74e26bf1463efb3b8499f1dc996754f4bb8
Innowidetech <innowidetech@gmail.com>
2024-11-07 15:34:28 +0530

    file upload

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-    cloud_name:process.env.cloud_name,
+    cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.CLOUDINARY_KEY,
     api_secret:process.env.CLOUDINARY_SECRET
 });
